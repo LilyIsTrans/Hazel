132c20fd72139c2fc9a46eb2fc8215aaa2697229
Rilazy <35211613+Rilazy@users.noreply.github.com>
2022-03-09 23:47:40 -0500

    Set up the entry point and basic project structure.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// FOR USE BY EXTERNAL CLIENT APPLICATIONS ONLY
+// HAZEL ITSELF SHOULD NEVER INCLUDE THIS FILE
+
+#include "Hazel/Application.h"
+
+// ---Entry Point--------------------------------
+#include "Hazel/EntryPoint.h"
